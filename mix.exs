da1c4eee5ac37834ea9bc6a596a5f903f7828e20
kanon75505601 <kanon75505601@yahoo.com.tw>
2019-01-01 05:47:01 +0800

    add Codecov on the yaml file for Travis CI

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Needed.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+      # if you want to use espec,
+      # test_coverage: [tool: ExCoveralls, test_task: "espec"]
     ]
   end
 
@@ -23,6 +32,7 @@ defmodule Needed.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 end
