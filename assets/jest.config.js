48a88a438e53a5ef5a5a38a33fe7e298278eb226
kanon75505601 <kanon75505601@yahoo.com.tw>
2019-07-29 19:21:32 +0800

    add(dev): add Enzyme (#32)

--- assets/jest.config.js
+++ assets/jest.config.js
@@ -5,5 +5,5 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
     "\\.(sa|sc|c)ss$": "<rootDir>/__mocks__/styleMock.js"
   },
-  testEnvironment: 'node'
+  setupFiles: ['./setup.js']
 };
